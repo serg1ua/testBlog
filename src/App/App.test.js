ba8f9fb7e36158fdd34b3c751a78d03979fa1ac4
serg1ua <marxser.ua@gmail.com>
2020-05-23 08:05:32 +0000

    App test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,15 +1,31 @@
-import { unmountComponentAtNode } from 'react-dom';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  let container = null;
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
+import App from './App.js';
 
-  afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders App without crashing', () => {
+  const app = shallow(
+    <Router>
+      <App />
+    </Router>);
+  act(() => {
+    render(<app />, container);
   });
 });
